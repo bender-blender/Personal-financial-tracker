ca2cacce58a54799a2d9956271a7bc4d4daa102f
Maxim <maksimgarsia709@gmail.com>
2026-07-07 00:05:18 +0300

    add database

---
+++ db/database.py
@@ -0,0 +1,63 @@
+import sqlite3
+
+
+
+class DataBase:
+    
+    def __init__(self):
+        self.bd = sqlite3.connect("finance.db")
+        self.cursor = self.bd.cursor()
+        self.cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS transactions (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        value REAL,
+        type TEXT NOT NULL
+        )
+        """)
+        self.bd.commit()
+    
+    def add_transaction(self, name:str, value:float, type_transaction="Доход"):
+        """
+        Добавить транзакцию
+        """
+        if type_transaction == "Расход":
+            value = -value
+        
+        self.cursor.execute(
+        """
+        INSERT INTO transactions (name, value, type)
+        VALUES (?, ?, ?)
+        """,(name,value,type_transaction))
+        self.bd.commit()
+
+    def remove_transaction(self, value:int):
+        """Удалить транзакцию
+        Можно по имени или индексу       
+        """
+        
+        if not isinstance(value,int):
+            raise Exception("value must be str or int")
+        
+
+        
+        self.cursor.execute(
+            """
+            DELETE FROM transactions WHERE id = ?
+            """,(value,)
+        )
+        self.bd.commit()
+        
+    
+    def get_everything(self):
+        """
+        Посмотреть все транзакции
+        """
+        data = {}
+        self.cursor.execute("SELECT * FROM transactions")
+
+        rows = self.cursor.fetchall()
+        for row in rows:
+            data[row[0]] = row[1:]  
+        return data
